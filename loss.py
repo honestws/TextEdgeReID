1a56d2938c59cf8dbaa4e1a889d0bd41155316a3
honestws <honestws@qq.com>
2023-09-09 21:32:17 +0800

    new

---
+++ loss.py
@@ -0,0 +1,67 @@
+import torch
+from torch import nn
+
+from utils import normalize, euclidean_dist, hard_example_mining
+
+
+def cross_entropy(preds, targets, reduction='none'):
+    log_softmax = nn.LogSoftmax(dim=-1)
+    loss = (-targets * log_softmax(preds)).sum(1)
+    if reduction == "none":
+        return loss
+    elif reduction == "mean":
+        return loss.mean()
+
+class TripletLoss(object):
+    """
+    Triplet loss using HARDER example mining,
+    modified based on original triplet loss using hard example mining
+    """
+
+    def __init__(self, margin=None, hard_factor=0.0):
+        self.margin = margin
+        self.hard_factor = hard_factor
+        if margin is not None:
+            self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+        else:
+            self.ranking_loss = nn.SoftMarginLoss()
+
+    def __call__(self, global_feat, labels, normalize_feature=False):
+        if normalize_feature:
+            global_feat = normalize(global_feat, axis=-1)
+        dist_mat = euclidean_dist(global_feat, global_feat) #B,B
+        dist_ap, dist_an = hard_example_mining(dist_mat, labels)
+
+        dist_ap *= (1.0 + self.hard_factor)
+        dist_an *= (1.0 - self.hard_factor)
+
+        y = dist_an.new().resize_as_(dist_an).fill_(1)
+        if self.margin is not None:
+            loss = self.ranking_loss(dist_an, dist_ap, y)
+        else:
+            loss = self.ranking_loss(dist_an - dist_ap, y)
+        return loss, dist_ap, dist_an
+
+
+class SupConLoss(nn.Module):
+    def __init__(self, device):
+        super(SupConLoss, self).__init__()
+        self.device = device
+        self.temperature = 1.0
+
+    def forward(self, text_features, image_features, t_label, i_labels):
+        batch_size = text_features.shape[0]
+        batch_size_N = image_features.shape[0]
+        mask = torch.eq(t_label.unsqueeze(1).expand(batch_size, batch_size_N),
+                        i_labels.unsqueeze(0).expand(batch_size, batch_size_N)).float().to(self.device)
+
+        logits = torch.div(torch.matmul(text_features, image_features.T),self.temperature)
+        # for numerical stability
+        logits_max, _ = torch.max(logits, dim=1, keepdim=True)
+        logits = logits - logits_max.detach()
+        exp_logits = torch.exp(logits)
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+        loss = - mean_log_prob_pos.mean()
+
+        return loss
